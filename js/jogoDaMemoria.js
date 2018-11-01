557e360df58a5bb48e7337e62df570056a0d48f2
gabrielfeba <gabrielfeba@outlook.com>
2018-11-01 16:59:49 -0300

    adicionar lógica de colocar as figuras embaralhadas.

--- js/jogoDaMemoria.js
+++ js/jogoDaMemoria.js
@@ -1,5 +1,50 @@
 const blocos = document.querySelectorAll("section>div");
-
+const blocosFrente = document.querySelectorAll("section>div>div:last-child");
+function embaralhar(lista) {
+    let novaLista = [];
+    let i = 0;
+    while(i < lista.length) {
+        let sortea = Math.floor(Math.random() * lista.length);
+        if(novaLista[sortea] == undefined) {
+            novaLista[sortea] = lista[i];
+            i++;
+        }
+    }
+    return novaLista;
+}
+function adicionarFigura() {
+    let figuras = [
+        "https://sc.mogicons.com/c/193.jpg",
+        "https://sc.mogicons.com/c/294.jpg",
+        "https://sc.mogicons.com/c/196.jpg",
+        "https://sc.mogicons.com/c/173.jpg",
+        "https://sc.mogicons.com/c/338.jpg",
+        "https://sc.mogicons.com/c/203.jpg",
+        "https://sc.mogicons.com/c/281.jpg",
+        "https://sc.mogicons.com/c/172.jpg",
+        "https://sc.mogicons.com/c/202.jpg",
+        "https://sc.mogicons.com/c/351.jpg",
+        "https://sc.mogicons.com/c/397.jpg",
+        "https://sc.mogicons.com/c/360.jpg",
+        "https://sc.mogicons.com/c/193.jpg",
+        "https://sc.mogicons.com/c/294.jpg",
+        "https://sc.mogicons.com/c/196.jpg",
+        "https://sc.mogicons.com/c/173.jpg",
+        "https://sc.mogicons.com/c/338.jpg",
+        "https://sc.mogicons.com/c/203.jpg",
+        "https://sc.mogicons.com/c/281.jpg",
+        "https://sc.mogicons.com/c/172.jpg",
+        "https://sc.mogicons.com/c/202.jpg",
+        "https://sc.mogicons.com/c/351.jpg",
+        "https://sc.mogicons.com/c/397.jpg",
+        "https://sc.mogicons.com/c/360.jpg"
+    ];
+    figuras = embaralhar(figuras);
+    console.log(figuras);
+    for(let indice in blocosFrente) {
+        blocosFrente[indice].style.backgroundImage = `url(${figuras[indice]})`;
+    }
+}
 function girar(event) {
     event.target.parentNode.style.transform = "rotateY(180deg)";
 }
@@ -7,3 +52,4 @@ function girar(event) {
 for(let bloco of blocos) {
     bloco.onclick = girar;
 }
+adicionarFigura();
